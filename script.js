3edddaa3da12d1f54962cd06b1780236e5ef0fef
PawanPawar11 <pawanpawar4369@gmail.com>
2024-06-10 11:01:36 +0530

    second commit

--- script.js
+++ script.js
@@ -3,11 +3,16 @@ function breakText() {
     var h1Text = h1.textContent
 
     var splittedText = h1Text.split("")
+    var halfValue = Math.floor(splittedText.length/2)
 
     var clutter = ""
 
-    splittedText.forEach(function(elem){
-        clutter += `<span>${elem}</span>`
+    splittedText.forEach(function(elem, idx){
+        if(idx<halfValue){
+            clutter += `<span class="a">${elem}</span>`
+        } else {
+            clutter += `<span class="b">${elem}</span>`
+        }
     })
 
     h1.innerHTML = clutter
@@ -15,10 +20,18 @@ function breakText() {
 
 breakText()
 
-gsap.from("h1 span", {
-    y:50,
-    opacity: 0,
-    duration:0.8,
+gsap.from("h1 .a", {
+    y:80,
+    duration:0.6,
     delay:0.5,
-    stagger: 0.2
+    stagger:0.15,
+    opacity:0
+})
+
+gsap.from("h1 .b", {
+    y:80,
+    duration:0.6,
+    delay:0.5,
+    stagger:-0.15,
+    opacity:0
 })
